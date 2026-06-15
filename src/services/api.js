fbbd335162ceada4d98820ae51b1d079194d7702
Gustavo Corrente <gustavomelo-1@hotmail.com>
2026-06-15 16:14:49 -0300

    troca da url do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://cadastro-users-backend-production.up.railway.app'
+    baseURL: 'https://cadastro-users-backend.onrender.com'
 });
 
 export default api;
